9e037dc467e22963fe2844b880628c2b2a95f02c
chenghengliang <2502915792@qq.com>
2019-07-09 17:30:40 +0800

    增加简单示例

---
+++ chapter1/src/main/java/descard/DiscardServerHandler.java
@@ -0,0 +1,45 @@
+package descard;
+
+/**
+ * @author chl
+ * @date 2019/7/9 17:15
+ */
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 处理服务端 channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // 默默地丢弃收到的数据
+       // ((ByteBuf) msg).release(); // (3)
+
+       /* ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }*/
+       /////////////
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
+        //////////
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
